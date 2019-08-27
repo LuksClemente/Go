3f11265ac5bc40de8860b18f3578d4969897b81b
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-08-27 17:41:51 -0300

    Add files via upload

---
+++ Tipos.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var altura = 1.34
+	
+	fmt.Printf("Tipo: %T\nValor: %v", altura, altura)
+}
---
+++ Variaveis.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var idade, altura int = 20, 178
+	var nome = "Lucas Clemente"
+	fmt.Println("O meu nome é", nome, "e tenho", idade, "anos!")
+	fmt.Println("Minha altura é", altura, "cm!")
+}
