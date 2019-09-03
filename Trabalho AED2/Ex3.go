448cfc6ce354933a57e55d336e5aee7e36049e4d
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-09-03 17:19:10 -0300

    Create Ex3.go

---
+++ Trabalho AED2/Ex3.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "math"
+
+func recursao(v []int, n int){
+	if(n < 2){
+		return
+	}
+	var nf float64 = float64(n)
+	var t int = int(math.Pow(2, nf))
+	V := make([]int, t) 
+	v1 := make([]int, n/2)
+	for i := 0; i < t; i = i + 1{
+		V[i] = i;
+		fmt.Printf("%d ->", V[i])
+	}
+	recursao(v1, n)
+}
+func main(){
+	v := make([]int, 4)
+	var n int = 4
+	recursao(v, n)
+}
