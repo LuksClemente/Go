473353631634eeb1609dee4ec6d9941d0b2577b2
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-09-06 19:33:37 -0300

    Update Ex7.go

--- Trabalho AED2/Ex7.go
+++ Trabalho AED2/Ex7.go
@@ -8,7 +8,7 @@ func recursao(v []int, n int){
 	}
 	v1 := make([]int, n/2)
 	v2 := make([]int, n/2)
-	for i := 0; i < n/2; i++{
+	for i := 0; i < n/2; i = i + 1{
 		v[i]++;
 	}
 	recursao(v1, n/2)
@@ -18,7 +18,5 @@ func main(){
 	v := make([]int, 10)
 	v = []int{2, 3, 5, 4, 1, 7, 8, 6, 9, 0}
 	recursao(v, 10)
-	for i := 0; i < 10; i++{
-		fmt.Printf("%d -> ", v[i])
-	}
+	fmt.Printf("%d", v)
 }
