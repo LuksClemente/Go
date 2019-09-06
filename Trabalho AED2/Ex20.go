9c6636c96bdf1f1fd4ceb0d66b14b72d180d23dd
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-09-06 19:31:07 -0300

    Update Ex20.go

--- Trabalho AED2/Ex20.go
+++ Trabalho AED2/Ex20.go
@@ -22,12 +22,10 @@ func recursao(v []int, n int){
 	recursao(v1, n/3)
 	recursao(v2, n/3)
 	recursao(v3, n/3)
-	recursao(v, n/3)
+	recursao(v1, n/3)
 }
 func main(){
 	v := make([]int, 27)
 	recursao(v, 15)
-	for i := 0; i < 27; i++{
-		fmt.Printf("%d -> ", v[i])
-	}
+	fmt.Printf("%d", v)
 }
