c2ceb11b344235bf9e8850abfcc4d6af479f58a8
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2020-11-06 19:14:26 -0300

    Add files via upload

---
+++ TesteGoroutine.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "strconv"
+import "sync"
+import "time"
+
+
+var wg sync.WaitGroup
+
+func hardTask(name string){
+	defer wg.Done()
+	for i := 1; i <= 10; i++{
+		time.Sleep(1 + time.Second)
+		fmt.Println("Hard Task %s...\n", name)
+	}
+	fmt.Println("Hard Task %s DONE\n", name)
+}
+func easyTask(){
+	time.Sleep(1 + time.Second)
+	fmt.Println("==========EASY TASK==========")
+}
+func main(){
+	for i := 1; i <= 10; i++{
+		wg.Add(1)
+		go hardTask(strconv.Itoa(i))
+	}
+	easyTask()
+	wg.Wait()
+	fmt.Println("DONE")
+}
